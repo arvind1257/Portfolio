766764593dc3347ded8138eb8d83bab2ce68a769
arvind1257 <97084780+arvind1257@users.noreply.github.com>
2025-01-31 12:11:03 +0530

    Created Portfolio in windows view

---
+++ js/main.js
@@ -0,0 +1,25 @@
+function switchTab1(index){
+    const tab  = document.querySelectorAll(".content-3 .tabLink");
+    const tabContent = document.querySelectorAll(".content-3 .sub-content");
+    for(let i=0;i<tab.length;i++){
+        tab[i].classList.remove("active");
+        tabContent[i].style.display="none";
+    }
+    tab[index-1].classList.add("active");
+    tabContent[index-1].style.display="block";
+}
+switchTab1(1);
+var current_tab = 1;
+window.addEventListener("scroll",()=>{
+    const tab  = document.querySelectorAll(".tab_link");
+    const tabContent = document.querySelectorAll("section");
+    for(let i=0;i<tabContent.length;i++){
+        if(window.scrollY>=(tabContent[i].offsetTop - tabContent[i].clientHeight/4)){
+            current_tab = i+1;
+        }
+        tab[i].classList.remove("active");
+    }
+    tab[current_tab-1].classList.add("active");
+
+
+})
